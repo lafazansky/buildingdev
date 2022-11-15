727d85976ec606c96873d542eed6d16a87858aad
lafazansky <v.lafazansky@gmail.com>
2022-11-15 15:40:00 +0200

    Adding menu and submenu for MV slider

--- app/public/wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
+++ app/public/wp-content/plugins/mv-slider/post-types/class.mv-slider-cpt.php
@@ -27,7 +27,7 @@ if(!class_exists('MV_Slider_Post_Type')){
                     ),
                     'hierarchical'          => false,
                     'show_ui'               => true,
-                    'show_in_menu'          => true,
+                    'show_in_menu'          => false,
                     'menu_position'         => 5,
                     'show_in_admin_bar'     => true,
                     'show_in_nav_menus'     => true,
